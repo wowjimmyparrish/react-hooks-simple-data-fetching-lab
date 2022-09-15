6ca6e2360bbb786f7333bfa026d4848b30c8e96e
James Parrish <jthomasparrishjr@gmail.com>
2022-09-14 19:02:42 -0500

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  // if the data hasn't been loaded, show a loading indicator
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={dogImage} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
